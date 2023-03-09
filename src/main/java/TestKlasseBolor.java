cd453d07d2c846c053185429b9f75bd6696f5bef
bolabolor <bolaskekse@gmail.com>
2023-03-09 12:19:08 +0100

    added sometext

--- src/main/java/TestKlasseBolor.java
+++ src/main/java/TestKlasseBolor.java
@@ -1,6 +1,7 @@
 public class TestKlasseBolor {
     public static void main(String[] args) {
         System.out.println("Hi Leute :)");
+        System.out.println("Bald kommt der Frühling yay :)");
     }
 
 }
